0d745e854fd4cbf6cea646991099ece38e203dc3
Maria Kim Heinert <yeeunmariakim@gmail.com>
2022-02-06 19:47:19 -0500

    Mock run function to avoid making requests

--- tests/test_command_green.py
+++ tests/test_command_green.py
@@ -1,10 +1,17 @@
 from typer.testing import CliRunner
 import toml
+import pytest
 
 from stoplight.run import app
 
 
 runner = CliRunner()
+
+
+@pytest.fixture(autouse=True)
+def patch_run(mocker):
+    mocker.patch('stoplight.run.run_green')
+    yield
 
 
 def test_green_errs_if_missing_token(mocker):
@@ -58,4 +65,40 @@ def test_green_does_not_err_if_assignment_in_rc(mocker):
     mocker.patch('builtins.open', mock_open)
     result = runner.invoke(
         app, ['green', '--token', 'test', '--org', 'test', 'test'])
+    assert result.exit_code == 0
+
+
+def test_green_errs_if_neither_all_nor_students_given(mocker):
+    mock_open = mocker.mock_open(read_data=toml.dumps({
+        'token': 'test',
+        'org': 'test',
+        'assignment_title': 'test'
+    }))
+    mocker.patch('builtins.open', mock_open)
+    result = runner.invoke(
+        app, ['green'])
+    assert result.exit_code == 1
+
+
+def test_green_does_not_err_if_all_given(mocker):
+    mock_open = mocker.mock_open(read_data=toml.dumps({
+        'token': 'test',
+        'org': 'test',
+        'assignment_title': 'test'
+    }))
+    mocker.patch('builtins.open', mock_open)
+    result = runner.invoke(
+        app, ['green', '--all'])
+    assert result.exit_code == 0
+
+
+def test_green_does_not_err_if_students_given(mocker):
+    mock_open = mocker.mock_open(read_data=toml.dumps({
+        'token': 'test',
+        'org': 'test',
+        'assignment_title': 'test'
+    }))
+    mocker.patch('builtins.open', mock_open)
+    result = runner.invoke(
+        app, ['green', 'testuser', 'testuser'])
     assert result.exit_code == 0
